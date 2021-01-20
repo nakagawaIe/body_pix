11f87df4bc7731169c4d54968633a35bc27c752b
中川拓哉 <takuya.nakagawa@ietty.co.jp>
2021-01-20 18:21:42 +0900

    fix: almost completed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-1. npm install<br>
-2. npm run dev<br>
+1. npm install
+2. npm run dev
 3. access http://localhost:8090/
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ import * as bodyPix from '@tensorflow-models/body-pix';
 import * as tf from '@tensorflow/tfjs';
 import { BodyPixInternalResolution } from '@tensorflow-models/body-pix/dist/types';
 
-tf.getBackend();
-
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const video = document.getElementById('video') as HTMLVideoElement;
-
 let net: bodyPix.BodyPix;
-let bodyPixMaks: ImageData;
+let effectTimeId: NodeJS.Timeout;
+let selectedType = 'image';
 
+const VIDEO_WIDTH = 1280;
+const VIDEO_HEIGHT = 720;
 
 const internalResolution: BodyPixInternalResolution = 'medium';
-const option = {
-  flipHorizontal: false,
+const flipHorizontal = true;
+const edgeBlurAmount = 5;
+const SEGMENT_OPTION = {
+  flipHorizontal,
   internalResolution,
   segmentationThreshold: 0.7,
   maxDetections: 4,
@@ -21,101 +21,134 @@ const option = {
   nmsRadius: 20,
 };
 
+const mainCanvas = document.getElementById('canvas') as HTMLCanvasElement;
+const mainCtx = mainCanvas.getContext('2d');
+mainCanvas.width = VIDEO_WIDTH;
+mainCanvas.height = VIDEO_HEIGHT;
 
-async function startVideo() {
-  const mediaConstraints = { video: { width: 640, height: 480 }, audio: false };
+const video = document.createElement('video');
+video.width = VIDEO_WIDTH;
+video.height = VIDEO_HEIGHT;
+
+let bgImage = document.querySelector('.images img') as HTMLImageElement;
+
+const bgCanvas = document.createElement('canvas');
+const bgCtx = bgCanvas.getContext('2d');
+bgCanvas.width = VIDEO_WIDTH;
+bgCanvas.height = VIDEO_HEIGHT;
+
+const constructor = () => {
+  tf.getBackend();
+  startVideo();
+};
+
+const startVideo = async () => {
+  const mediaConstraints = { video: { width: VIDEO_WIDTH, height: VIDEO_HEIGHT }, audio: false };
   const localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
   video.srcObject = localStream;
   await video.play();
-  net = await bodyPix.load(/** optional arguments, see below **/);
+  net = await bodyPix.load();
   if (!net) {
     console.warn('bodyPix net NOT READY');
     return;
   }
-  // window.requestAnimationFrame(updateCanvas);
-  // updateSegment();
-  window.requestAnimationFrame(detectBody);
-  detectBody();
-}
-startVideo();
+  factoryEffect(selectedType);
+};
 
-function updateCanvas() {
-  drawCanvas();
-  window.requestAnimationFrame(updateCanvas);
-}
+const factoryEffect = (type?: string) => {
+  if (type === 'off') {
+    effectCallback(offEffect);
+  } else if (type === 'color') {
+    effectCallback(colorEffect);
+  } else if (type === 'image') {
+    effectCallback(bgImageEffect);
+  } else {
+    effectCallback(bokehEffect);
+  }
+};
 
-function drawCanvas() {
+const effectCallback = (callback: () => void) => {
+  window.requestAnimationFrame(callback);
+  callback();
+  effectTimeId = setInterval(callback, 10);
+};
+
+const offEffect = () => {
+  mainCtx.drawImage(video, 0, 0, mainCanvas.width, mainCanvas.height);
+};
+
+const bokehEffect = () => {
+  const backgroundBlurAmount = 10;
+  net.segmentPerson(video, SEGMENT_OPTION).then(s => {
+    bodyPix.drawBokehEffect(mainCanvas, video, s, backgroundBlurAmount, edgeBlurAmount, flipHorizontal);
+  });
+};
+
+const colorEffect = () => {
   const opacity = 1;
-  const flipHorizontal = false;
-  const maskBlurAmount = 10;
-  bodyPix.drawMask(canvas, video, bodyPixMaks, opacity, maskBlurAmount, flipHorizontal);
-}
-
-async function updateSegment() {
-  net.segmentPerson(video, option).then((segmentation) => {
+  net.segmentPerson(video, SEGMENT_OPTION).then((segmentation) => {
     const fgColor = { r: 0, g: 0, b: 0, a: 0 };
-    const bgColor = { r: 255, g: 255, b: 255, a: 200 };
+    const bgColor = { r: 0, g: 0, b: 0, a: 220 };
     const personPartImage = bodyPix.toMask(segmentation, fgColor, bgColor);
-    bodyPixMaks = personPartImage;
-
-    drawBody(bodyPixMaks);
+    bodyPix.drawMask(mainCanvas, video, personPartImage, opacity, edgeBlurAmount, flipHorizontal);
   }).catch((err) => {
     console.error('segmentPerson ERROR:', err);
   });
-}
+};
 
+const bgImageEffect = () => {
+  net.segmentPerson(video, SEGMENT_OPTION).then(s => drawToCanvas(s));
+};
 
-const canvasContext = canvas.getContext('2d');
-
-function detectBody() {
-  net.segmentPerson(video, option).then(personSegmentation => {
-    if (personSegmentation != null) {
-      drawBody(personSegmentation);
-    }
-  });
-}
-
-function drawBody(personSegmentation: bodyPix.SemanticPersonSegmentation) {
-  // function drawBody(personSegmentation: ImageData) {
-  canvasContext.drawImage(video, 0, 0, video.width, video.height);
-  const imageData = canvasContext.getImageData(0, 0, video.width, video.height);
-  const pixel = imageData.data;
-  for (let p = 0; p < pixel.length; p += 4) {
-    if (personSegmentation.data[p / 4] == 0) {
-      pixel[p + 3] = 0;
-    }
-  }
-  canvasContext.imageSmoothingEnabled = true;
-  canvasContext.putImageData(imageData, 0, 0);
-  setTimeout(detectBody, 10);
-  // setTimeout(updateSegment, 10);
-}
-
-
-function drawToCanvas(canvas, segmentation, img, data_img, data_bg) {
-  const ctx = canvas.getContext('2d');
-  canvas.width = img.width;
-  canvas.height = img.height;
-  const width = img.width;
-  const height = img.height;
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const pixels = imageData.data;
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const base = (y * width + x) * 4;
-      const segbase = y * width + x;
-      if (segmentation.data[segbase] == 1) { // is fg
-        pixels[base + 0] = data_img.data[base + 0];
-        pixels[base + 1] = data_img.data[base + 1];
-        pixels[base + 2] = data_img.data[base + 2];
-        pixels[base + 3] = data_img.data[base + 3];
-      } else {
-        pixels[base + 0] = data_bg.data[base + 0];
-        pixels[base + 1] = data_bg.data[base + 1];
-        pixels[base + 2] = data_bg.data[base + 2];
-        pixels[base + 3] = data_bg.data[base + 3];
+const drawToCanvas = (segmentation: bodyPix.SemanticPersonSegmentation) => {
+  mainCtx.drawImage(video, 0, 0, video.width, video.height);
+  const imageData = mainCtx.getImageData(0, 0, video.width, video.height);
+  bgCtx.drawImage(bgImage, 0, 0, mainCanvas.width, mainCanvas.height);
+  const bgCtxImage = bgCtx.getImageData(0, 0, mainCanvas.width, mainCanvas.height);
+  for (let y = 0; y < VIDEO_HEIGHT; y++) {
+    for (let x = 0; x < VIDEO_WIDTH; x++) {
+      const base = (y * VIDEO_WIDTH + x) * 4;
+      const segbase = y * VIDEO_WIDTH + x;
+      if (segmentation.data[segbase] !== 1) { // is bg
+        imageData.data[base + 0] = bgCtxImage.data[base + 0];
+        imageData.data[base + 1] = bgCtxImage.data[base + 1];
+        imageData.data[base + 2] = bgCtxImage.data[base + 2];
+        imageData.data[base + 3] = bgCtxImage.data[base + 3];
       }
     }
   }
-  ctx.putImageData(imageData, 0, 0);
+  mainCtx.putImageData(imageData, 0, 0);
+};
+
+
+constructor();
+
+bgImage.classList.add('is-active');
+document.querySelector(`[data-type="${selectedType}"]`).classList.add('is-active');
+
+const buttons = document.querySelectorAll('.buttons li');
+for (let i = 0; i < buttons.length; i++) {
+  const button = buttons[i] as HTMLElement;
+  button.onclick = () => {
+    buttons.forEach(b => b.classList.remove('is-active'));
+    button.classList.add('is-active');
+    selectedType = button.dataset.type;
+    restartEffect();
+  };
 }
+
+const images = document.querySelectorAll('.images img');
+for (let i = 0; i < images.length; i++) {
+  const image = images[i] as HTMLImageElement;
+  image.onclick = () => {
+    images.forEach(b => b.classList.remove('is-active'));
+    image.classList.add('is-active');
+    bgImage = image;
+    restartEffect();
+  };
+}
+
+const restartEffect = () => {
+  clearTimeout(effectTimeId);
+  factoryEffect(selectedType);
+};
