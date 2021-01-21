dd5fd7dd2ffeb77474b9790bed61a4f9cf9f91d1
中川拓哉 <takuya.nakagawa@ietty.co.jp>
2021-01-21 18:42:46 +0900

    fix: add capture video

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,44 @@
 import './style.scss';
 import { VirtualBgClass } from './virtual_bg';
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const imagesWrap = document.querySelector('.images') as HTMLUListElement;
-const bgImage = document.querySelector('.images img') as HTMLImageElement;
-const bgEffect = new VirtualBgClass(canvas, bgImage);
+window.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  const imagesWrap = document.querySelector('.images') as HTMLUListElement;
+  const bgImage = document.querySelector('.images img') as HTMLImageElement;
+  const bgEffect = new VirtualBgClass(canvas, bgImage);
 
-const buttons = document.querySelectorAll('.buttons li');
-document.querySelector(`[data-type="${bgEffect._effectType}"]`).classList.add('is-active');
+  const buttons = document.querySelectorAll('.buttons li');
+  document.querySelector(`[data-type="${bgEffect._effectType}"]`).classList.add('is-active');
 
-const imageActive = () => imagesWrap.style.display = bgEffect._effectType === 'image' ? 'flex' : 'none';
-imageActive();
+  const imageActive = () => imagesWrap.style.display = bgEffect._effectType === 'image' ? 'flex' : 'none';
+  imageActive();
 
-for (let i = 0; i < buttons.length; i++) {
-  const button = buttons[i] as HTMLElement;
-  button.onclick = () => {
-    buttons.forEach(b => b.classList.remove('is-active'));
-    button.classList.add('is-active');
-    bgEffect._effectType = button.dataset.type;
-    imageActive();
-    bgEffect.restartEffect();
-  };
-}
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i] as HTMLElement;
+    button.onclick = () => {
+      buttons.forEach(b => b.classList.remove('is-active'));
+      button.classList.add('is-active');
+      bgEffect._effectType = button.dataset.type;
+      imageActive();
+      bgEffect.restartEffect();
+    };
+  }
 
-const images = document.querySelectorAll('.images img');
-bgImage.classList.add('is-active');
-for (let i = 0; i < images.length; i++) {
-  const image = images[i] as HTMLImageElement;
-  image.onclick = () => {
-    images.forEach(b => b.classList.remove('is-active'));
-    image.classList.add('is-active');
-    bgEffect.changeBgImage(image);
-  };
-}
+  const images = document.querySelectorAll('.images img');
+  bgImage.classList.add('is-active');
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i] as HTMLImageElement;
+    image.onclick = () => {
+      images.forEach(b => b.classList.remove('is-active'));
+      image.classList.add('is-active');
+      bgEffect.changeBgImage(image);
+    };
+  }
+
+  const capture = document.getElementById('capture') as HTMLVideoElement;
+  // 適当に5秒待つ
+  setTimeout(() => {
+    capture.srcObject = bgEffect.getStream();
+    capture.play();
+  }, 5000);
+});
